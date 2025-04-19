165e621cb39304b6d5f78a4e911d8187440fd222
backgamon <39404647+backgamon@users.noreply.github.com>
2025-04-19 15:32:03 +0200

    Add files via upload

--- src/video_core/renderer_opengl/gl_state.h
+++ src/video_core/renderer_opengl/gl_state.h
@@ -22,11 +22,12 @@ constexpr TextureUnit PicaTexture(int unit) {
     return TextureUnit{unit};
 }
 
+constexpr TextureUnit TextureCube{6};
 constexpr TextureUnit TextureBufferLUT_LF{3};
 constexpr TextureUnit TextureBufferLUT_RG{4};
 constexpr TextureUnit TextureBufferLUT_RGBA{5};
-constexpr TextureUnit TextureNormalMap{6};
-constexpr TextureUnit TextureColorBuffer{7};
+constexpr TextureUnit TextureNormalMap{7};
+constexpr TextureUnit TextureColorBuffer{10};
 
 } // namespace TextureUnits
 
@@ -94,11 +95,14 @@ public:
     // 3 texture units - one for each that is used in PICA fragment shader emulation
     struct TextureUnit {
         GLuint texture_2d; // GL_TEXTURE_BINDING_2D
-        GLenum target;     // GL_TEXTURE_TARGET
         GLuint sampler;    // GL_SAMPLER_BINDING
     };
     std::array<TextureUnit, 3> texture_units;
 
+    struct {
+        GLuint texture_cube; // GL_TEXTURE_BINDING_CUBE_MAP
+        GLuint sampler;      // GL_SAMPLER_BINDING
+    } texture_cube_unit;
     struct {
         GLuint texture_buffer; // GL_TEXTURE_BINDING_BUFFER
     } texture_buffer_lut_lf;
